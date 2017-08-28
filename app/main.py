b3e546da805dad6fdf9b2a4c3627344e5dd41bc5
Joe Corbett <joseph.corbett@gmail.com>
2017-08-28 15:02:13 +1200

    using phantom js

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def load_parameters():
     pickle_file = os.path.join(os.path.dirname(__file__), 'data', 'tf_parameters.pkl')
     with open(pickle_file, 'rb') as f:
         parameters = pickle.load(f, encoding='latin1')
-
+        #
     return parameters
 
 def compute_relu(W, A, b):
